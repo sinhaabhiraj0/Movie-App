d6214d71fcd34124448bba9c05542ce5c97c7bd0
Abhiraj <sinhaabhiraj0@gmail.com>
2021-03-20 19:30:24 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const request = require('request');
+const app=express();
+
+//Middlewares
+app.set("view engine","ejs");
+app.use('/public',express.static('public'));
+
+app.get('/', (req,res)=>{
+    //res.send('Home Page-Abhiraj');
+    res.render("home");
+})
+app.get('/result', (req,res)=>{
+    console.log(req.query)
+    const url=`http://www.omdbapi.com/?apikey=63e9460&s=${req.query.movieName}`;
+    request(url,function(error,response,body){
+        if(!error && response.statusCode===200){
+            const data=JSON.parse(body);
+            //res.send(data)
+            res.render("result",{moviesDump:data});
+        }else{
+            res.send('Something went wrong');
+        }
+    })
+})
+
+app.get('/result/:id', (req,res)=>{
+    const url=`http://www.omdbapi.com/?apikey=63e9460&i=${req.params.id}`;
+    request(url,function(error,response,body){
+        if(!error && response.statusCode===200){
+            const data=JSON.parse(body);
+            //res.send(data);
+            res.render("details",{data:data});
+        }else{
+            res.send('Something went wrong');
+        }
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.send("404 not found");
+})    
+app.listen(3000, ()=>{
+    console.log('server has started');
+})
